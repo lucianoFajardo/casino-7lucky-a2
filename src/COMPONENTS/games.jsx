cb2353dc2fc3d3bd10651ec4db5d5207c94a68a2
luciano <desmondangel@gmail.com>
2023-06-06 14:29:31 -0400

    guardado avance

--- src/COMPONENTS/games.jsx
+++ src/COMPONENTS/games.jsx
@@ -4,7 +4,7 @@ import { CardGamesFree } from "../COMPONENTS/card_view_games";
 export function GamesViewCustom() {
 
     const FreeGamesView = CardGamesFree.map(i =>
-    <div className="flex bg-purple">
+    <div className="flex justify-start bg-purple">
         <div key={i.id}  className="m-10">
             <div className="card card-compact bg-base-500 shadow-x1 w-100">
                 <figure><img src={i.img} alt="Juego de azar" /></figure>
@@ -20,7 +20,7 @@ export function GamesViewCustom() {
     </div>);
 
     return (
-        <div className="bg-gray-dark grid grid-cols-1 p-4 gap-5 mx-auto sm:gap-5 sm:flex-col sm:grid-cols-1 sm:p-5 sm:justify-center md:flex md:flex-col 2xl:flex-row">
+        <div className="bg-gray-dark grid grid-cols-1 p-4 gap-5 mx-auto sm:gap-5 sm:flex-col sm:grid-cols-1 sm:p-5 sm:justify-center md:flex md:flex-col 2xl:flex-row 2xl:grid-cols-2">
             {FreeGamesView}
         </div>
     );
