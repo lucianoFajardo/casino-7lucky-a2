6b42937ff20640768c19514df76ce0d5c46f72df
luciano <desmondangel@gmail.com>
2023-06-07 21:02:59 -0400

    actualizado 07-06-23 v.04

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { BodyView } from './VIEW/body';
-import { FooterView } from './VIEW/footer';
-import { HeaderView } from './VIEW/header';
 
 function App() {
   return (
